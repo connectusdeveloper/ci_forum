594c0ce0285f66b984ebfcff7b9fe3366795fd01
Ajay Nayak <connectusdemo12@gmail.com>
2023-09-28 12:20:19 +0530

    git changes upload

--- resources/views/vendor/mail/html/header.blade.php
+++ resources/views/vendor/mail/html/header.blade.php
@@ -2,11 +2,15 @@
 <tr>
   <td class="header">
     <a href="{{ $url }}" style="display: inline-block;">
+		<?php /*
       @if ($settings['logo'] ?? null)
         <img src="{{ asset($settings['logo']) }}" class="logo" alt="Laravel Logo">
       @else
         {{ $slot }}
       @endif
+	  */
+		?>
+		<h3 style="text-align:center;">Emerald Isle Homes</h3>
     </a>
   </td>
 </tr>
